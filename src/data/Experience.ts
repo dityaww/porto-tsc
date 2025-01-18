5cb3538b43b5795f99d12e720a9f9f80ae98ffb8
dityaww <111202012838@mhs.dinus.ac.id>
2025-01-18 22:26:58 +0700

    add experience data

---
+++ src/data/Experience.ts
@@ -0,0 +1,38 @@
+export const DataExperience = [
+    {
+        id: 1,
+        name: 'PT. Evolusi Bisnis Digital',
+        date: 'Jan 2024 - Aug 2024',
+        position: 'Web Programmer',
+        desc: 'Collaborate with Backend Developer as Frontend Developer to create a responsive user interface using ReactJS. Serve as a note takerand create MOM (Minutes of Meeting) documents during meetings with clients. Create test script documents used for SIT, UAT, and TO activities'
+    },
+    {
+        id: 2,
+        name: 'PT. Ruang Raya Indonesia',
+        date: 'Jan 2023 - Jun 2023',
+        position: 'Independent Study Front-End Engineering',
+        desc: 'Learn Software Project Management and Administration. Learning the basics of website creation with HTML and CSS. Exploring CSS libraries (Tailwind CSS). Gaining knowledge of fundamental JavaScript programming concepts such as functions, conditionals, asynchronous JavaScript, fetching data APIs (fetch and axios), displaying data using DOM manipulation methods, and REST APIs. Learning state management using Redux (redux-toolkit). Learn JavaScript libraries like ReactJS, including React hooks, React router, state management with Redux and Context, and fetching API data using axios and fetch. Projects: Library Room Application for managing book data with a CRUD system, Portfolio Website, Student Portal Application.'
+    },
+    {
+        id: 3,
+        name: 'PT. Impactbyte Teknologi Edukasi (Skilvul)',
+        date: 'Aug 2022 - Des 2022',
+        position: 'Independent Study Front-End Web Development',
+    },
+    {
+        id: 4,
+        name: 'Digital Talent Scholarship KOMINFO X Cisco Networking Academy',
+        date: 'Jul 2022 - Aug 2022',
+        position: 'Fresh Graduate Academy Big Data using Python',
+        desc: 'Learn to use the Python programming language such as data types, conditions, functions, tuples, dictionaries, data processing, and OOP in Python. Learn modules and packages in Python. Learn Python GUI namely Tkinter. Learn Machine Learnings, Internet of Things, and Data Analysis with Python'
+    },
+    {
+        id: 5,
+        name: 'PT. Sadasa Academy Indonesia',
+        date: 'Jul 2021 - Sep 2021',
+        position: 'Social Networking in R',
+        desc: 'Learning social media analysis preprocessing. Learning the use of R Programming. Exploring the use of Igraph and Gephi. Understanding the concepts of Centrality and Modularity.'
+    },
+]
+
+// 
