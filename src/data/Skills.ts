8d74087b5127adfee27353ec957b0efe73825600
dityaww <111202012838@mhs.dinus.ac.id>
2025-01-18 22:27:24 +0700

    export skills assets

---
+++ src/data/Skills.ts
@@ -0,0 +1,65 @@
+import {
+    HTMLIcon,
+    CSSIcon,
+    JSIcon,
+    BootstrapsIcon,
+    TailwindIcon,
+    ReactJSIcon,
+    JiraIcon,
+    FigmaIcon,
+    GitIcon,
+    PostmanIcon
+} from '../assets/skills'
+
+export const DataSkills = [
+    {
+        id: 1,
+        name: 'HTML',
+        image: HTMLIcon,
+    },
+    {
+        id: 2,
+        name: 'CSS',
+        image: CSSIcon,
+    },
+    {
+        id: 3,
+        name: 'Javascript',
+        image: JSIcon,
+    },
+    {
+        id: 4,
+        name: 'Bootstraps',
+        image: BootstrapsIcon,
+    },
+    {
+        id: 5,
+        name: 'Tailwind CSS',
+        image: TailwindIcon,
+    },
+    {
+        id: 6,
+        name: 'React JS',
+        image: ReactJSIcon,
+    },
+    {
+        id: 7,
+        name: 'Jira',
+        image: JiraIcon,
+    },
+    {
+        id: 8,
+        name: 'Figma JS',
+        image: FigmaIcon,
+    },
+    {
+        id: 9,
+        name: 'Git',
+        image: GitIcon,
+    },
+    {
+        id: 10,
+        name: 'Postman',
+        image: PostmanIcon,
+    },
+]
