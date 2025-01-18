bbe6c718e5ec895892abead95347132da246a504
dityaww <111202012838@mhs.dinus.ac.id>
2025-01-18 22:28:10 +0700

    add projects data

---
+++ src/data/Projects.ts
@@ -0,0 +1,13 @@
+import { TypeProject } from "../types/projects/type";
+
+import { SunrisePay } from "../assets/projects";
+
+export const DataProjects:TypeProject[]  = [
+    {
+        id: 1,
+        projectName: 'Sunrise Pay',
+        description: 'lorem ipsum',
+        images: SunrisePay,
+        link: 'wwq'
+    }
+]
