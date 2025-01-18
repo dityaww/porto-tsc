fa1cba29e975eb9fc09dae6aac324ec22c671614
dityaww <111202012838@mhs.dinus.ac.id>
2025-01-18 22:30:14 +0700

    export icons

---
+++ src/assets/skills/index.tsx
@@ -0,0 +1,23 @@
+import HTMLIcon from './html.svg'
+import CSSIcon from './css.svg'
+import JSIcon from './js.svg'
+import BootstrapsIcon from './bootstrap.svg'
+import TailwindIcon from './tailwindcss.svg'
+import ReactJSIcon from './reactjs.svg'
+import JiraIcon from './jira.svg'
+import FigmaIcon from './figma.svg'
+import GitIcon from './git.svg'
+import PostmanIcon from './postman.svg'
+
+export {
+    HTMLIcon,
+    CSSIcon,
+    JSIcon,
+    BootstrapsIcon,
+    TailwindIcon,
+    ReactJSIcon,
+    JiraIcon,
+    FigmaIcon,
+    GitIcon,
+    PostmanIcon
+}
