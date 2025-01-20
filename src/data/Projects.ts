ea607d0e765cfb06adca619fe7261d6ba49c0697
dityaww <111202012838@mhs.dinus.ac.id>
2025-01-20 20:53:05 +0700

    add some projects

--- src/data/Projects.ts
+++ src/data/Projects.ts
@@ -1,13 +1,41 @@
 import { TypeProject } from "../types/projects/type";
 
-import { SunrisePay } from "../assets/projects";
+import { Properly, SunrisePay, MoviesApp, BMICalculator, AYOBantu } from "../assets/projects";
 
 export const DataProjects:TypeProject[]  = [
     {
         id: 1,
         projectName: 'Sunrise Pay',
-        description: 'lorem ipsum',
+        description: '-',
         images: SunrisePay,
-        link: 'wwq'
-    }
+        link: 'https://sunrisepay.vercel.app'
+    },
+    {
+        id: 2,
+        projectName: 'Properly',
+        description: '-',
+        images: Properly,
+        link: '#'
+    },
+    {
+        id: 3,
+        projectName: 'Movie App',
+        description: '-',
+        images: MoviesApp,
+        link: 'https://react-movie-tmdb.vercel.app'
+    },
+    {
+        id: 4,
+        projectName: 'BMI Calculator',
+        description: '-',
+        images: BMICalculator,
+        link: 'https://bmi-calc-project.netlify.app'
+    },
+    {
+        id: 5,
+        projectName: 'Ayo Bantu Web',
+        description: '-',
+        images: AYOBantu,
+        link: 'https://ayobantu-webs.vercel.app'
+    },
 ]
