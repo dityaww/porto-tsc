00ec8cd5a2a558e97b08c083e6086aa636c796b8
dityaww <111202012838@mhs.dinus.ac.id>
2025-01-20 20:53:14 +0700

    export assets

--- src/assets/projects/index.tsx
+++ src/assets/projects/index.tsx
@@ -1,5 +1,13 @@
 import SunrisePay from './spay.jpg'
+import Properly from './properly.png'
+import MoviesApp from './movies.png'
+import BMICalculator from './BMICalculator.png'
+import AYOBantu from './AYOBantu.png'
 
 export {
-    SunrisePay
+    SunrisePay,
+    Properly,
+    MoviesApp,
+    BMICalculator,
+    AYOBantu
 }
